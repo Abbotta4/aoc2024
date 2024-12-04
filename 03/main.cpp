d2a2de4a068769c5addf884f1fe8da79e41f5b54
Drew Abbott <abbotta4@gmail.com>
2024-12-04 01:40:24 +0100

    Day 3 part 2

--- 03/main.cpp
+++ 03/main.cpp
@@ -16,21 +16,19 @@ int part1(std::string const& input) {
     int sum = 0;
     std::regex muls(R"(mul\(([0-9]+),([0-9]+)\))");
     for (auto i = std::sregex_token_iterator(input.begin(), input.end(), muls, {1, 2}); i != std::sregex_token_iterator(); ++i) {
-	int tmp = std::stoi((i++)->str());
-	tmp *= std::stoi(i->str());
-	sum += tmp;
+        int tmp = std::stoi((i++)->str());
+        tmp *= std::stoi(i->str());
+        sum += tmp;
     }
     return sum;
 }
 
-int part2(std::string const& input) {
+int part2(std::string& input) {
+    input = std::regex_replace(input, std::regex(R"((?:\r\n|\n))"), ""); // newlines break doDont
     std::regex doDont(R"((?:do\(\)|^)(.*?)(?:don't\(\)|$))");
     std::string newInput = "";
-    for (auto i = std::sregex_token_iterator(input.begin(), input.end(), doDont, {0}); i != std::sregex_token_iterator(); ++i) {
-        //std::cout << "found" << i->str() << std::endl;
+    for (auto i = std::sregex_token_iterator(input.begin(), input.end(), doDont, {0}); i != std::sregex_token_iterator(); ++i)
         newInput += i->str();
-    }
-    //std::cout << newInput << std::endl;
     return part1(newInput);
 }
 
