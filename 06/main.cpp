8d7037ddb3116193f2acf558c67c32e982381544
Drew Abbott <abbotta4@gmail.com>
2024-12-12 17:38:03 +0100

    Add broken day 6 part 2

--- 06/main.cpp
+++ 06/main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <set>
+#include <algorithm>
 
 class DirectionCycle {
 private:
@@ -19,6 +20,9 @@ public:
     }
     std::pair<int, int>* operator->() {
 	return &cycle[index];
+    }
+    std::pair<int, int> peek() const {
+	return cycle[(index+1)%4];
     }
 };
 
@@ -62,8 +66,49 @@ int part1(std::vector<std::string> const& input) {
     return visited.size();
 }
 
+bool checkObstruction(std::set<std::tuple<int, int, int, int>> const& visited, int x, int y, std::pair<int, int> const& direction) {
+    auto [dx, dy] = direction;
+
+    if (dx == -1)
+	return visited.end() != std::find_if(visited.begin(), visited.end(), [&](auto v){ return (std::get<1>(v) == y and std::get<0>(v) < x) and std::get<2>(v) == dx and std::get<3>(v) == dy; });
+    if (dx == 1)
+	return visited.end() != std::find_if(visited.begin(), visited.end(), [&](auto v){ return (std::get<1>(v) == y and std::get<0>(v) > x) and std::get<2>(v) == dx and std::get<3>(v) == dy; });
+    if (dy == -1)
+	return visited.end() != std::find_if(visited.begin(), visited.end(), [&](auto v){ return (std::get<1>(v) < y and std::get<0>(v) == x) and std::get<2>(v) == dx and std::get<3>(v) == dy; });
+    //if (dy == 1)
+    return visited.end() != std::find_if(visited.begin(), visited.end(), [&](auto v){ return (std::get<1>(v) > y and std::get<0>(v) == x) and std::get<2>(v) == dx and std::get<3>(v) == dy; });
+}
+
+int part2(std::vector<std::string> const& input) {
+    int obstructionPositions = 0;
+    auto [x, y] = findStartingPosition(input);
+    std::set<std::tuple<int, int, int, int>> visited; // coord, direction
+    auto direction = DirectionCycle();
+    while (x >= 0 and x < input[0].size() and y >= 0 and y < input.size()) {
+	if (input[y][x] == '#') {
+	    std::tie(x, y) = std::make_pair(x - direction->first, y - direction->second);
+	    ++direction;
+	    continue;
+	}
+	visited.insert({x, y, direction->first, direction->second});
+	auto peekDirection = direction.peek();
+	std::cout << "checking for {" << x + peekDirection.first << ", " << y + peekDirection.second << ", " << peekDirection.first << ", " << peekDirection.second << "}" << std::endl;
+	std::cout << "visited: {" << std::endl;
+	for (auto m: visited) {
+	    std::cout << "    {" << std::get<0>(m) << ", " << std::get<1>(m) << ", " << std::get<2>(m) << ", " << std::get<3>(m) << "}" << std::endl;
+	}
+	if (checkObstruction(visited, x, y, peekDirection)) {
+	    std::cout << "good spot at (" << x + direction->first << ", " << y + direction->second << ")" << std::endl;
+	    obstructionPositions++;
+	}
+	std::tie(x, y) = std::make_pair(x + direction->first, y + direction->second);
+    }
+    return obstructionPositions;
+}
+
 int main() {
     std::vector<std::string> input = readInput("input.txt");
     std::cout << part1(input) << std::endl;
+    std::cout << part2(input) << std::endl;
     return 0;
 }
